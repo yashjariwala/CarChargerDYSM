a7541a6f3b63af2925a70246d05d660a7f9e7c4d
Yash Jariwala <yashjariwala@gmail.com>
2021-03-28 23:20:02 +0530

    Linked to Arduino Commit files
    Basic Linking done Style sheeting pending

--- app/src/main/java/com/dysm/carchargerdysm/startcharging.java
+++ app/src/main/java/com/dysm/carchargerdysm/startcharging.java
@@ -1,11 +1,36 @@
 package com.dysm.carchargerdysm;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class startcharging extends MainActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startcharging);
+        Button ledon = findViewById(R.id.ledon);
+        ledon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myref = database.getReference().child("LED_STATUS");
+                myref.setValue(0);
+            }
+        });
+        Button ledoff = findViewById(R.id.ledoff);
+        ledoff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myref = database.getReference().child("LED_STATUS");
+                myref.setValue(1);
+
+            }
+        });
     }
 }
