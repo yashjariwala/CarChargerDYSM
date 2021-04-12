462e257e62f153e026721b37b6b376755796b572
Yash Jariwala <yashjariwala@gmail.com>
2021-04-12 14:42:36 +0530

    Welcome Screen Redesign
    Welcome screen animated

--- app/src/main/java/com/dysm/carchargerdysm/WelcomeScreen.java
+++ app/src/main/java/com/dysm/carchargerdysm/WelcomeScreen.java
@@ -2,41 +2,63 @@ package com.dysm.carchargerdysm;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 import static com.dysm.carchargerdysm.R.layout.activity_main;
+import static com.dysm.carchargerdysm.R.layout.bottomlayout;
 import static com.dysm.carchargerdysm.R.layout.welcomscreen;
 
 public class WelcomeScreen extends Activity {
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(welcomscreen);
-        int[] imageArray = {
-                R.drawable.startup1, R.drawable.startup2, R.drawable.startup3, R.drawable.bg
-        };
-        Handler handler = new Handler();
-        Runnable runnable = new Runnable() {
-            int i = 0;
-
-            public void run() {
-                ImageView imageView5 = findViewById(R.id.imageView4);
-                imageView5.setImageResource(imageArray[i]);
-                i++;
-                if (i > (imageArray.length) | i == (imageArray.length)){
-                    startActivity(new Intent(WelcomeScreen.this, MainActivity.class));
-                    System.exit(0);
-                }
-                handler.postDelayed(this, 1000);
-            }
-        };
-        handler.postDelayed(runnable, 1000);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Animation animation;
+        ImageView lightsoncar = findViewById(R.id.welcomescreencarlights);
+        ImageView mat = findViewById(R.id.welcomescreenchargermatoff);
+        TextView name = findViewById(R.id.welcomescreename);
+        animation = AnimationUtils.loadAnimation(this,R.anim.slideinsmoothstartcharging);
+        lightsoncar.startAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) { }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                lightsoncar.setImageResource(R.drawable.carlightsoff);
+                mat.setVisibility(View.GONE);
+                name.setVisibility(View.VISIBLE);
+                 Handler handler = new Handler();
+                 handler.postDelayed(new Runnable() {
+                     @Override
+                     public void run() {
+                         startActivity(new Intent(WelcomeScreen.this,MainActivity.class));
+                     }
+                 },1500);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+    }
 
 }
+
+
